2bedc2336edd7cf59e0a4aa20e51b09a8ab5455a
TonyAnthony1 <tonyanthonyyyyy@gmail.com>
2026-06-11 13:43:29 +0200

    update Program.cs

--- HospitalApi/Program.cs
+++ HospitalApi/Program.cs
@@ -1,23 +1,44 @@
+using System.Text.Json.Serialization;
+using HospitalApi.Data;
+using HospitalApi.Repositories;
+using HospitalApi.Services;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<HospitalContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPatientRepo, PatientRepo>();
+builder.Services.AddScoped<IBedAssignmentRepo, BedAssignmentRepo>();
+
+
+builder.Services.AddScoped<IPatientService, PatientServ>();
+builder.Services.AddScoped<IBedAssignmentServ, BedAssignmentService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+    });
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    
     app.MapOpenApi();
+    
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
