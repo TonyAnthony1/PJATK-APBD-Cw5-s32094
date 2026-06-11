732c9e1062a7687c22c27ccd3ad137014b501b55
TonyAnthony1 <tonyanthonyyyyy@gmail.com>
2026-06-11 13:45:02 +0200

    Added Exceptions

---
+++ HospitalApi/Services/DomainExceptions.cs
@@ -0,0 +1,13 @@
+namespace HospitalApi.Services;
+
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message) { }
+}
+
+
+public class ValidationException : Exception
+{
+    public ValidationException(string message) : base(message) { }
+}
